378be2e51d420a0db814ced023a8f4e371120fdd
Postanker <91223637+Postanker@users.noreply.github.com>
2023-11-19 18:18:00 +0300

    Добавьте файлы проекта.

---
+++ WebTriinkom/Controllers/RegistrationController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTriinkom.Controllers
+{
+    public class RegistrationController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
